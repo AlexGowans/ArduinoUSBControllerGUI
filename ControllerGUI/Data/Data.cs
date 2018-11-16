5410b97fd463c81c95e332c6fca0dbf3ec3d51cd
Alex Gowans <alexgowans@hotmail.co.uk>
2018-11-16 19:53:56 +0000

    gg

--- ControllerGUI/Data/Data.cs
+++ ControllerGUI/Data/Data.cs
@@ -7,6 +7,20 @@ using System.Threading.Tasks;
 namespace ControllerGUI {
     class Data {
 
+        public int mode { get; set; }
+   
+        public ushort upBtn { get; set; }
+        public ushort leftBtn { get; set; }
+        public ushort downBtn { get; set; }
+        public ushort rightBtn { get; set; }
+
+        public ushort aBtn { get; set; }
+        public ushort bBtn { get; set; }
+        public ushort xBtn { get; set; }
+        public ushort yBtn { get; set; }
+        public ushort lBtn { get; set; }
+        public ushort rBtn { get; set; }
+
 
     }
 }

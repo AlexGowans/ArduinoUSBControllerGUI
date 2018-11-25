4ffa880c2dc039d8bc94015a6481a3bc787ea9a5
AlexGowans <alexgowans@hotmail.co.uk>
2018-11-25 09:07:21 -0800

    Gui Dropdowns

--- ControllerGUI/MainPage.xaml.cs
+++ ControllerGUI/MainPage.xaml.cs
@@ -56,7 +56,59 @@ namespace ControllerGUI {
         private async void btnWrite_Click(object sender, RoutedEventArgs e) {
             serialController.PreparePacketSend(txtSend.Text.ToString());
         }
-        #endregion
+        
 
+        private void btnDCDropdown_Click(object sender, RoutedEventArgs e) {
+            if (spDC.Visibility == Visibility.Collapsed)
+            {
+                spDC.Visibility = Visibility.Visible;
+                btnDCDropdown.Content = "v";
+            }
+            else if (spDC.Visibility == Visibility.Visible)
+            {
+                spDC.Visibility = Visibility.Collapsed;
+                btnDCDropdown.Content = ">";
+            }
+        }
+
+        private void btnGVDropdown_Click(object sender, RoutedEventArgs e) {
+            if (spGV.Visibility == Visibility.Collapsed)
+            {
+                spGV.Visibility = Visibility.Visible;
+                btnGVDropdown.Content = "v";
+            }
+            else if (spGV.Visibility == Visibility.Visible)
+            {
+                spGV.Visibility = Visibility.Collapsed;
+                btnGVDropdown.Content = ">";
+            }
+        }
+
+        private void btnDVDropdown_Click(object sender, RoutedEventArgs e)
+        {
+            if(spDV.Visibility == Visibility.Collapsed) {
+                spDV.Visibility = Visibility.Visible;
+                btnDVDropdown.Content = "v";
+            }
+            else if (spDV.Visibility == Visibility.Visible)
+            {
+                spDV.Visibility = Visibility.Collapsed;
+                btnDVDropdown.Content = ">";
+            }
+        }
+        
+        private void btnSDropdown_Click(object sender, RoutedEventArgs e) {
+            if (spS.Visibility == Visibility.Collapsed)
+            {
+                spS.Visibility = Visibility.Visible;
+                btnSDropdown.Content = "v";
+            }
+            else if (spS.Visibility == Visibility.Visible)
+            {
+                spS.Visibility = Visibility.Collapsed;
+                btnSDropdown.Content = ">";
+            }
+        }
+        #endregion
     }
 }
